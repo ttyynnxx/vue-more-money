9bde0ceafbfbd5b139a290c06207272ccd46ca0e
ty要nx <ttyy373172968@163.com>
2021-04-03 17:55:08 +0800

    创建了 Nav 样式&解决了 svg自带样式覆盖问题

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,18 +1,19 @@
+import Icon from '@/components/Icon.vue';
 <template>
-  <div class="nav">
-    <router-link to="/money">
+  <nav class="nav">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="标签" />
       记账</router-link
     >
-    <router-link to="/labels">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="竖状图" />
       标签
     </router-link>
-    <router-link to="/statistics">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="淘宝客推广" />
       统计</router-link
     >
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -23,7 +24,24 @@ export default {
 
 <style lang="scss" scoped>
 .nav {
-  border: 1px solid red;
   display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  flex-direction: row;
+  font-size: 12px;
+  > .item {
+    padding: 6px 0;
+    width: 33.33%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
+  > .item.selected {
+    color: red;
+  }
 }
 </style>
