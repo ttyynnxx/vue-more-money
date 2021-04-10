cfc88bd6c3fed440bf65c5c60fc9a07619d141de
ty要nx <ttyy373172968@163.com>
2021-04-10 14:42:05 +0800

    修改二维码的地址

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path =require('path')
 
 module.exports ={
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-money-website'
+    ? '/vue-money-website/'
     : '/',
     lintOnSave: false,
     chainWebpack: config =>{
